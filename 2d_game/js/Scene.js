cd9b961b4ce899f053b8db69d5ac76515140ec78
acsbendi <acsbendi28@gmail.com>
2019-03-03 22:43:01 +0100

    Added many game objects around Avatar.

--- 2d_game/js/Scene.js
+++ 2d_game/js/Scene.js
@@ -10,11 +10,8 @@ const Scene = function (gl) {
 
   this.material.colorTexture.set(
     new Texture2D(gl, 'media/asteroid.png'));
-  this.material2.colorTexture.set(
-    new Texture2D(gl, 'media/boom.png'));
   this.material.texOffset.set(0.1, 0.4);
   this.mesh = new Mesh(this.quadGeometry, this.material);
-  this.mesh2 = new Mesh(this.quadGeometry, this.material2);
 
   this.timeAtFirstFrame = new Date().getTime();
   this.timeAtLastFrame = this.timeAtFirstFrame;
@@ -23,26 +20,45 @@ const Scene = function (gl) {
 
   this.gameObjects = [];
 
-  let gameObject1 = new GameObject(this.mesh);
-  gameObject1.position = new Vec3(0, 0.4, 0);
-  gameObject1.scale = new Vec3(0.1, 0.1, 0.1);
-  gameObject1.move = function (t, dt, keysPressed, gameObjects) {
-    this.orientation = t;
-  };
-  this.gameObjects.push(gameObject1);
+  for (let i = -2.3; i < 1.8; i += 0.22) {
+    let gameObject = new GameObject(this.mesh);
+    gameObject.position = new Vec3(i, 1.21, 0);
+    gameObject.scale = new Vec3(0.1, 0.1, 0.1);
+    gameObject.move = function (t, dt, keysPressed, gameObjects) {
+      this.orientation = t;
+      this.handleCollisions(gameObjects);
+    };
+    this.gameObjects.push(gameObject);
+  }
+
+  for (let i = 1; i > -0.7; i -= 0.22) {
+    let gameObject = new GameObject(this.mesh);
+    gameObject.position = new Vec3(-2.3, i, 0);
+    gameObject.scale = new Vec3(0.1, 0.1, 0.1);
+    gameObject.move = function (t, dt, keysPressed, gameObjects) {
+      this.orientation = t;
+      this.handleCollisions(gameObjects);
+    };
+    this.gameObjects.push(gameObject);
+  }
+
+
+  for (let i = -2.3; i < 2; i += 0.22) {
+    let gameObject = new GameObject(this.mesh);
+    gameObject.position = new Vec3(i, -0.76, 0);
+    gameObject.scale = new Vec3(0.1, 0.1, 0.1);
+    gameObject.move = function (t, dt, keysPressed, gameObjects) {
+      this.orientation = t;
+      this.handleCollisions(gameObjects);
+    };
+    this.gameObjects.push(gameObject);
+  }
 
   let gameObject2 = new GameObject(this.mesh);
-  gameObject2.position = new Vec3(0.6, -0.7, 0);
+  gameObject2.position = new Vec3(0.6, -0.45, 0);
   gameObject2.scale = new Vec3(0.2, 0.1, 0.1);
   gameObject2.speed = new Vec3(1, 1, 0);
   this.gameObjects.push(gameObject2);
-
-  let gameObject3 = new GameObject(this.mesh2);
-  gameObject3.position = new Vec3(0.4, 0, 0);
-  gameObject3.scale = new Vec3(0.1, 0.1, 0.3);
-  gameObject3.acceleration = new Vec3(10.1, 60.1, 0);
-  this.gameObjects.push(gameObject3);
-
 
   this.avatar = new Avatar(gl, this.solidProgram, this.quadGeometry);
   this.gameObjects.push(this.avatar);
@@ -80,8 +96,8 @@ Scene.prototype.createNewBullet = function () {
   bullet.position = new Vec3(this.avatar.position);
   bullet.mass = 0.1;
   bullet.scale = new Vec3(0.1, 0.1, 0.1);
-  let speedX = Math.cos(this.avatar.orientation + Math.PI/2);
-  let speedY = Math.sin(this.avatar.orientation + Math.PI/2);
+  let speedX = Math.cos(this.avatar.orientation + Math.PI / 2);
+  let speedY = Math.sin(this.avatar.orientation + Math.PI / 2);
   bullet.speed = new Vec3(speedX, speedY).times(10, 10);
   this.gameObjects.push(bullet);
 };
